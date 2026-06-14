9e8d38cbd9814a9bcbe3c2b66493b48377dec95f
igorkrets <igorkrets@yandex.ru>
2026-06-14 05:44:40 +0000

    chore: scaffold laravel project with vite tailwind alpine

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import Alpine from 'alpinejs';
+import Sortable from 'sortablejs';
+import Fuse from 'fuse.js';
+
+window.Sortable = Sortable;
+window.Fuse = Fuse;
+window.Alpine = Alpine;
+
+Alpine.start();
